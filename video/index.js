8349e419b5afffa6c85ab77815868a55941fcdee
Kiran P <kiran.p@bazaarvoice.com>
2024-03-01 20:03:08 +0530

    aaa

---
+++ video/index.js
@@ -0,0 +1,164 @@
+let mediaRecorder;
+let chunks = []
+const videoEle = document.querySelector("#live")
+const videoSrcEle = document.querySelector("#videoSource")
+const output = document.querySelector("#output")
+const inputFileEle = document.querySelector("#inputfile")
+const resultDiv = document.querySelector("#permissionresult")
+const errorResultDiv = document.querySelector("#permissionresult")
+const createBtn1 = document.querySelector("#createbtn1")
+const createBtn2 = document.querySelector("#createbtn2")
+const stopBtn = document.querySelector("#stopbtn")
+const options = {
+    // mimeType: "video/webm"
+}
+
+// alert("Init")
+
+function stopCamera(stream) {
+    if (stream) {
+        stream.getTracks().forEach((track) => {
+            /* console.log("stop for each track : ", track.stop) */
+            track.stop();
+        })
+        /* console.log("After stop : ", runningStream.getTracks()) */
+    }
+
+}
+
+function initialize(stream) {
+    chunks = []
+    window.stream = stream;
+    mediaRecorder = new MediaRecorder(window.stream, options);
+
+    mediaRecorder.ondataavailable = (e) => {
+        chunks.push(e.data);
+    }
+    console.log("State: ", mediaRecorder.state)
+    mediaRecorder.onerror = (event) => {
+        console.log("Media recorder ERROR : ", event.error);
+    }
+
+    mediaRecorder.onstop = () => {
+        stopCamera(window.stream);
+        console.log("Final chunks : ", chunks)
+        const blob = new Blob(chunks, options)
+        const url = URL.createObjectURL(blob)
+        videoSrcEle.src = url;
+        videoEle.srcObject = null;
+        videoEle.controls = true;
+        videoEle.loop = true;
+        videoEle.load()
+        videoEle.play().then(() => {
+
+        }).catch(e => {
+            console.log("Error on play() : ", e)
+        });
+    };
+
+    videoSrcEle.src = null;
+    videoEle.controls = false;
+    videoEle.loop = false;
+    videoEle.srcObject = window.stream;
+}
+
+function create(e) {
+    inputFileEle.click()
+}
+
+function create2(e) {
+    console.log("Create clicked!")
+    navigator.mediaDevices
+        .getUserMedia({
+            video: true,
+            audio: true
+        })
+        .then((stream) => {
+            initialize(stream)
+            console.log("Starting the recording ...")
+            console.log("MimeType : ", mediaRecorder.mimeType)
+
+            mediaRecorder.start(1000);
+
+        })
+        .catch((err) => {
+            logPermissions(['camera', 'microphone'])
+            console.log("Error : ", err)
+        });
+
+
+}
+
+function stop(e) {
+    console.log("Stopping ....")
+    mediaRecorder.stop()
+    console.log("State: ", mediaRecorder.state)
+}
+
+function handleFileInput(e) {
+    const uploadedFile = e?.target?.files[0];
+    if (uploadedFile) {
+        console.log("Type of uploaded file : ", typeof e?.target?.files[0] );
+        const url = URL.createObjectURL(uploadedFile)
+        videoEle.srcObject = null;
+        videoEle.controls = true;
+        videoEle.loop = true;
+        videoSrcEle.src = url;
+        videoEle.load()
+        // videoEle.play().then(() => {
+
+        // }).catch(e => {
+        //     alert("Error on file input play")
+        //     console.log("Error on file input play : ", e)
+        // });
+        prepareToUpload(uploadedFile) 
+    } else {
+        alert("Uploaded file not found!")
+    }
+}
+
+function prepareToUpload(file) {
+    const filePromise = new Promise(resolve => {
+        const reader = new FileReader()
+        reader.onload = () => {
+          resolve({ localUrl: reader.result, blob: file })
+        }
+        reader.readAsDataURL(file)
+      })
+    
+      filePromise.then(results => {
+        console.log("File reader results : ", results);
+      })
+}
+
+async function logPermissions(list = []) {
+    const state1 = await check(list[0])
+    const state2 = await check(list[1])
+    console.log({ [list[0]]: state1, [list[1]]: state2 });
+    // alert(`storage : ${storage} and camera : ${camera}`)
+    resultDiv.innerHTML = `${list[0]} : ${state1} and ${list[1]} : ${state2}`
+}
+
+async function checkFileInput(e) {
+    logPermissions(['camera', 'storage-access'])
+
+}
+
+
+async function check(type = '') {
+    try {
+        const result = await navigator.permissions.query({ name: type })
+        return result.state
+    } catch (e) {
+        errorResultDiv.innerHTML = `Permission query Error: ${e}`
+    }
+    // resultDiv.innerHTML = `Result of ${type} : ${result.state}`
+    // alert(`Result of ${type} : ${result.state}`)
+}
+
+
+createBtn1.addEventListener('click', create)
+createBtn2.addEventListener('click', create2)
+stopBtn.addEventListener('click', stop)
+inputFileEle.addEventListener('change', handleFileInput)
+inputFileEle.addEventListener('click', checkFileInput)
---
+++ video/upload/index.js
@@ -0,0 +1,167 @@
+let mediaRecorder;
+let chunks = []
+const videoEle = document.querySelector("#live")
+const videoSrcEle = document.querySelector("#videoSource-mp4")
+const output = document.querySelector("#output")
+const inputFileEleCreate = document.querySelector("#inputfilecreate")
+const inputFileEleUpload = document.querySelector("#inputfileupload")
+const inputfilee = document.querySelector("#inputfile")
+
+const resultDiv = document.querySelector("#permissionresult")
+const errorResultDiv = document.querySelector("#permissionresult")
+const createBtn = document.querySelector("#createBtn")
+const uploadBtn = document.querySelector("#uploadBtn")
+const stopBtn = document.querySelector("#stopbtn")
+const selectedFileNameEle = document.querySelector("#selectedFileName")
+const submitBtn = document.querySelector("#submit")
+const videoLinkEle = document.querySelector("#videolink")
+let uploadedFile = null;
+const options = {
+    // mimeType: "video/mp4"
+}
+let currentType = 'mp4'
+// alert("Init")
+
+function getVideoSourceEle(type = 'video/mp4') {
+    let type1 = 'mp4'
+    switch (type) {
+        case 'video/mp4':
+            type1 = 'mp4';
+            break;
+        case 'video/avi':
+            type1 = 'mov';
+            break;
+
+        case 'video/quicktime':
+            type1 = 'mov'
+            break;
+
+        case 'video/mpeg':
+            type1 = 'mpeg'
+            break;
+        case 'video/3gpp2':
+            type1 = '3gpp2'
+            break;
+
+
+    }
+    currentType = type
+    return document.querySelector(`#videoSource-${type1}`)
+}
+function create(type = 'create') {
+    if (type === 'create') {
+
+        inputfilee.click()
+    } else {
+        inputFileEleUpload.click()
+    }
+}
+
+
+function handleFileInput(e) {
+    const uploadedFile = e?.target?.files[0];
+    if (uploadedFile) {
+        videoLinkEle.style.display = "none"
+        selectedFileNameEle.innerHTML = uploadedFile.name || 'Unknown'
+        let srcEle = getVideoSourceEle(currentType)
+        srcEle.src = null
+        // reset src value for prev video type
+
+        videocontainer.style.display = "block"
+        console.log("Type of uploaded file : ", uploadedFile.type);
+        const url = URL.createObjectURL(uploadedFile)
+        srcEle = getVideoSourceEle(uploadedFile.type)
+        videoEle.srcObject = null;
+        videoEle.controls = true;
+        videoEle.loop = true;
+        srcEle.src = url;
+        videoEle.load()
+        // videoEle.play().then(() => {
+
+        // }).catch(e => {
+        //     alert("Error on file input play")
+        //     console.log("Error on file input play : ", e)
+        // });
+        prepareToUpload(uploadedFile)
+    } else {
+        alert("Uploaded file not found!")
+    }
+}
+
+function prepareToUpload(file) {
+    const filePromise = new Promise(resolve => {
+        const reader = new FileReader()
+        reader.onload = () => {
+            resolve({ localUrl: reader.result, blob: file })
+        }
+        reader.readAsDataURL(file)
+    })
+
+    filePromise.then(result => {
+        uploadedFile = result;
+        console.log("File ready to be submitted to API : ", result.blob);
+    })
+}
+
+
+function handleVideoError(e) {
+    console.log("Video error : ", e
+    );
+}
+
+function handleEvent(event) {
+    console.log(`${event.type}`);
+}
+
+function submit() {
+    var data = new FormData();
+    data.append("apiversion", "5.5");
+    data.append("contenttype", "review");
+    data.append("video", uploadedFile.blob);
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            const res = this.responseText && JSON.parse(this.responseText)
+            console.log("API Response : ", res);
+            if (res) {
+                videoLinkEle.style.display = "block"
+                videoLinkEle.href = res.Video.VideoUrl
+                videoLinkEle.style.color = "green"
+                videoLinkEle.style.fontWeight = "bold"
+            }
+        }
+    });
+    xhr.open("POST", "https://qamedia.api.bazaarvoice.com/data/uploadvideo.json?PassKey=caVCg5HE3ecxMzeEpHKHF8seU6ixGfW8dVoiAkGJjA3HQ&displayCode=13928_15_0");
+
+    xhr.send(data);
+}
+
+function loadedmetadata(e) {
+    console.log("loadedmetadata duration: ", videoEle.duration);
+}
+
+function handleCancel() {
+    console.log("Cancelled");
+    alert("Cancelled")
+
+}
+
+createBtn.addEventListener('click', create)
+uploadBtn.addEventListener('click', () => create('upload'))
+inputFileEleCreate.addEventListener('change', handleFileInput)
+inputFileEleCreate.addEventListener('cancel', handleCancel)
+inputFileEleUpload.addEventListener('change', handleFileInput)
+inputFileEleUpload.addEventListener('cancel', handlhandleCanceleFileInput)
+videoEle.addEventListener('error', handleVideoError)
+videoEle.addEventListener('waiting', handleEvent)
+videoEle.addEventListener('loadeddata', handleEvent)
+videoEle.addEventListener('suspend', handleEvent)
+videoEle.addEventListener('stalled', handleEvent)
+videoEle.addEventListener("progress", handleEvent);
+videoEle.addEventListener("canplay", handleEvent);
+videoEle.addEventListener("canplaythrough", handleEvent);
+videoEle.addEventListener("loadedmetadata", loadedmetadata);
+submitBtn.addEventListener('click', submit)
+
